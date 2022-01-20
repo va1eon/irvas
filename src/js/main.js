1f6cec754bd55271bc81db17958160a082673ecb
Valery Mikhailov <va1eonmix@gmail.com>
2022-01-20 18:43:21 +0900

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
 import './slider';
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
   modals();
+  tabs({
+    tabSelector: '.glazing_block',
+    headerSelector: '.glazing_slider',
+    contentSelector: '.glazing_content',
+    activeClass: 'active',
+  });
+
+  tabs({
+    tabSelector: '.no_click',
+    headerSelector: '.decoration_slider',
+    contentSelector: '.decoration_content > div > div',
+    activeClass: 'after_click',
+  });
 });
